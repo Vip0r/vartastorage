34e2cb72ac46c77a6190a7806dc05124ab12c60f
Andy <Andy@macbook.ndnet>
2021-12-26 20:39:07 +0100

    first release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="vartastorage",
-    version="0.1.0",
+    version="0.1.1",
     description="VARTA Battery",
     url="http://github.com/vip0r/vartastorage",
     author="edAndy",
