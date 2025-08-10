ea7eb6afe0e83be491fc1f31152bc191dcd4f95f
edAndy <an-dy@gmx.de>
2025-08-10 17:06:38 +0200

    Update setup.py
    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="vartastorage",
-    version="0.4.0",
+    version="0.4.1",
     description="VARTA Battery",
     long_description="""
         With this Python module you can read modbus registers
